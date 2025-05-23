7f0be33196a1df886105f1756b0894c91e23e20a
Ziri <45337531+ziriraha@users.noreply.github.com>
2025-05-23 18:20:06 +0200

    app hello world

--- app/run.py
+++ app/run.py
@@ -28,19 +28,19 @@ app.register_blueprint(views_bp, url_prefix='/')
 logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
-    # Verifying if the dataset zip files exist
-    # If they don't exist, they are created
-    if os.path.exists(ZIP_DIRECTORY):
-        for dataset in DATASET_NAMES:
-            if not os.path.exists(ZIP_DIRECTORY + dataset + '.zip'):
-                make_dataset_zip(dataset)
-    else: 
-        os.makedirs(ZIP_DIRECTORY)
-        for dataset in DATASET_NAMES:
-            make_dataset_zip(dataset)
+    # # Verifying if the dataset zip files exist
+    # # If they don't exist, they are created
+    # if os.path.exists(ZIP_DIRECTORY):
+    #     for dataset in DATASET_NAMES:
+    #         if not os.path.exists(ZIP_DIRECTORY + dataset + '.zip'):
+    #             make_dataset_zip(dataset)
+    # else: 
+    #     os.makedirs(ZIP_DIRECTORY)
+    #     for dataset in DATASET_NAMES:
+    #         make_dataset_zip(dataset)
 
-    # Creating the custom datasets directory if it doesn't exists
-    os.makedirs(CUSTOM_DIRECTORY, exist_ok=True)
+    # # Creating the custom datasets directory if it doesn't exists
+    # os.makedirs(CUSTOM_DIRECTORY, exist_ok=True)
 
     logger.info('App started')
     app.run(debug=True)
--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,3 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views_bp = Blueprint('views', __name__)
+
+@views_bp.route('/')
+def index():
+    return render_template('index.html')
